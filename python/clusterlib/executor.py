b84da62e3d7dfccec8b01648b2409616c8b0e100
willemm <Willem.Marais@ssec.wisc.edu>
2019-10-15 22:05:44 +0000

    Renamed a file.

--- python/clusterlib/executor.py
+++ python/clusterlib/executor.py
@@ -6,7 +6,6 @@ import secrets
 import importlib
 import clusterlib.file as Tfile
 import clusterlib.makeflow as makeflow
-from clusterlib.wrapexe import IrisWrapperExecute
 from typing import Callable, Dict, List, Tuple, Union
 
 
@@ -83,7 +82,7 @@ class Stage(StageAbstract):
                  graph_stage_dct: Dict[str, Tuple[StageAbstract, StageAbstractCollection_type]],
                  name_str: str,
                  py_func: Callable,
-                 input_parm_obj_dct: Dict[str, Union[int, float, str, StageFile]],
+                 input_parm_obj_dct: Dict[str, Union[int, float, str, StageFile, List[StageFile]]],
                  output_file_dct: Dict[str, StageFile]):
         """
 
@@ -118,8 +117,9 @@ class Stage(StageAbstract):
             raise ValueError(err_str)
 
         def _check_stage_present(_stage_obj):
-            err_str = f'The stage with name "{_stage_obj.name_str}" does not exists in the graph.'
-            raise ValueError(err_str)
+            if _stage_obj.name_str not in graph_stage_dct:
+                err_str = f'The stage with name "{_stage_obj.name_str}" does not exists in the graph.'
+                raise ValueError(err_str)
 
         # Make sure that the input stages already exist in the graph
         if input_stage_obj is not None:
@@ -144,6 +144,7 @@ class Stage(StageAbstract):
 
     def crt_makeflow_rule(self,
                           parm_dirP_fileP_str: str,
+                          py_caller_script_fileP_str: str,
                           wrapper_bash_scrpt_fileP_str: str,
                           cat_obj: makeflow.Category = None) -> makeflow.Rule:
         """Create a makeflow rule.
@@ -153,6 +154,8 @@ class Stage(StageAbstract):
         parm_dirP_fileP_str: str
             The file path of the directory for a file which will contain the parameters
             of the function that will be executed.
+        py_caller_script_fileP_str: str
+            The file path location where the python wrapper script is stored.
         wrapper_bash_scrpt_fileP_str: str
             The wrapper bash script that will call the corresponding stage.
         cat_obj: makeflow.Category
@@ -178,18 +181,54 @@ class Stage(StageAbstract):
             'output_fileP_str_dct': dict()
         }
 
-        # Populate the keyword arguments of the function; also keep a list of input files
-        for input_parm_name_str, input_parm_value_obj in self.input_parm_obj_dct.items():
-            if isinstance(input_parm_value_obj, StageInputFile) is True:
-                parm_dct['function_kwargs'][input_parm_name_str] = str(input_parm_value_obj)
-                parm_dct['input_fileP_str_dct'][input_parm_name_str] = str(input_parm_value_obj)
+        def transform_StageFile_objects(input_name_str, input_obj):
+            _output_obj = None
+            _input_fileP_str_dct = dict()
+            _output_fileP_str_dct = dict()
 
-            elif isinstance(input_parm_value_obj, StageOutputFile) is True:
-                parm_dct['function_kwargs'][input_parm_name_str] = str(input_parm_value_obj)
-                parm_dct['output_fileP_str_dct'][input_parm_name_str] = str(input_parm_value_obj)
+            if isinstance(input_obj, StageInputFile) is True:
+                _output_obj = str(input_obj)
+                _input_fileP_str_dct[input_name_str] = str(input_obj)
+
+            elif isinstance(input_obj, StageOutputFile) is True:
+                _output_obj = str(input_obj)
+                _output_fileP_str_dct[input_name_str] = str(input_obj)
+
+            elif isinstance(input_obj, list) is True:
+                _output_obj = list()
+                for idx, _input_obj in enumerate(input_obj):
+                    _input_name_str = f'{input_name_str}_{idx}'
+                    __output_obj, __input_fileP_str_dct, __output_fileP_str_dct = \
+                        transform_StageFile_objects(_input_name_str, _input_obj)
+
+                    _output_obj.append(__output_obj)
+                    _input_fileP_str_dct.update(__input_fileP_str_dct)
+                    _output_fileP_str_dct.update(__output_fileP_str_dct)
+
+            elif isinstance(input_obj, dict) is True:
+                _output_obj = dict()
+                for key_str, _input_obj in input_obj.items():
+                    _input_name_str = f'{input_name_str}_{key_str}'
+                    __output_obj, __input_fileP_str_dct, __output_fileP_str_dct = \
+                        transform_StageFile_objects(_input_name_str, _input_obj)
+
+                    _output_obj[key_str] = __output_obj
+                    _input_fileP_str_dct.update(__input_fileP_str_dct)
+                    _output_fileP_str_dct.update(__output_fileP_str_dct)
 
             else:
-                parm_dct['function_kwargs'][input_parm_name_str] = input_parm_value_obj
+                _output_obj = input_obj
+
+            return _output_obj, _input_fileP_str_dct, _output_fileP_str_dct
+
+        # Populate the keyword arguments of the function; also keep a list of input files
+        for input_parm_name_str, input_parm_value_obj in self.input_parm_obj_dct.items():
+            output_obj, input_fileP_str_dct, output_fileP_str_dct = transform_StageFile_objects(input_parm_name_str,
+                                                                                                input_parm_value_obj)
+
+            parm_dct['function_kwargs'][input_parm_name_str] = output_obj
+            parm_dct['input_fileP_str_dct'].update(input_fileP_str_dct)
+            parm_dct['output_fileP_str_dct'].update(output_fileP_str_dct)
 
         # Write out the parameter yaml file
         with Tfile.TFileTo(fileP_str=parm_fileP_str) as tfile_obj:
@@ -197,12 +236,18 @@ class Stage(StageAbstract):
                 yaml.dump(parm_dct, stream=file_obj)
 
         # Make a list of output files that are not present in self.output_file_dct
-        for name_str, stage_file_obj in self.output_file_dct:
-            parm_dct['output_fileP_str_dct'][name_str] = str(stage_file_obj)
+        if self.output_file_dct is not None:
+            for name_str, stage_file_obj in self.output_file_dct:
+                parm_dct['output_fileP_str_dct'][name_str] = str(stage_file_obj)
+
+        # Create the python wrapper script
+        with open(py_caller_script_fileP_str, 'w') as file_obj:
+            file_obj.write('import sys\n')
+            file_obj.write('import clusterlib.executor as executor\n')
+            file_obj.write(f'executor.Stage.execute(sys.argv[1])\n')
 
         # Create the command string
-        cmd_str = f'{wrapper_bash_scrpt_fileP_str} -c ' + \
-            f'\'import clusterlib.executor as executor; executor.Stage.execute({parm_fileP_str})\''
+        cmd_str = f'/bin/bash {wrapper_bash_scrpt_fileP_str} {py_caller_script_fileP_str} {parm_fileP_str}'
 
         # Create the input and output file lists
         input_fileP_str_lst = [parm_fileP_str] + list(parm_dct['input_fileP_str_dct'].values())
@@ -222,7 +267,7 @@ class Stage(StageAbstract):
         param_dct = None
         with Tfile.TFileFrom(fileP_str=parm_fileP_str) as tfile_obj:
             with open(tfile_obj.local_fileP_str, 'r') as file_obj:
-                param_dct = yaml.load(stream=file_obj)
+                param_dct = yaml.load(stream=file_obj, Loader=yaml.FullLoader)
 
         # Load the appropriate module
         module_obj = importlib.import_module(param_dct['module_path'])
@@ -235,7 +280,7 @@ class Stage(StageAbstract):
 
         # Create a list of transcended input files
         in_tfile_obj_lst = []
-        for name_str, input_fileP_str in param_dct['input_fileP_str_dct']:
+        for name_str, input_fileP_str in param_dct['input_fileP_str_dct'].items():
             tfile_obj = Tfile.TFileFrom(fileP_str=input_fileP_str)
             in_tfile_obj_lst.append(tfile_obj)
 
@@ -244,7 +289,7 @@ class Stage(StageAbstract):
 
         # Create a list of transcended output files
         out_tfile_obj_lst = []
-        for name_str, output_fileP_str in param_dct['output_fileP_str_dct']:
+        for name_str, output_fileP_str in param_dct['output_fileP_str_dct'].items():
             tfile_obj = Tfile.TFileTo(fileP_str=output_fileP_str)
             out_tfile_obj_lst.append(tfile_obj)
 
@@ -260,16 +305,13 @@ class Stage(StageAbstract):
 class MakeflowFromStages:
     """Create the makeflow file from the collection of stages."""
 
-    def __init__(self, parm_dirP_str: str,
-                 wrapper_bash_scrpt_fileP_str: str,
-                 cat_obj: makeflow.Category = None):
+    def __init__(self, parm_dirP_str: str, wrapper_bash_scrpt_fileP_str: str, cat_obj: makeflow.Category = None):
         """
 
         Parameters
         ----------
         parm_dirP_str: str
-            The parameter directory path of where to archive the
-            parameter YAML files for the stage execution.
+            The parameter directory path of where to archive the parameter YAML files for the stage execution.
         wrapper_bash_scrpt_fileP_str: str
             The iris bash wrapper script executable.
         cat_obj: makeflow.Category
@@ -279,19 +321,26 @@ class MakeflowFromStages:
         self.wrapper_bash_scrpt_fileP_str = wrapper_bash_scrpt_fileP_str
         self.cat_obj = cat_obj
 
-    def create(self, graph_stage_dct: Dict[str, Tuple[StageAbstract, StageAbstractCollection_type]], out_fileP_str: str):
+    def create(self,
+               graph_stage_dct: Dict[str, Tuple[StageAbstract, StageAbstractCollection_type]],
+               makeflow_out_fileP_str: str,
+               py_caller_script_fileP_str: str):
         """Create the Makeflow JX file.
 
         Parameters
         ----------
         graph_stage_dct: Dict[str, Tuple[StageAbstract, StageAbstractCollection_type]]
             The graph of the stages.
-        out_fileP_str: str
-            The output file path of the makeflow file."""
+        makeflow_out_fileP_str: str
+            The output file path of the makeflow file.
+        py_caller_script_fileP_str: str
+            The file path location where the python wrapper script is stored.
+        """
 
         # Create the keyword dictionary for the makeflow rule function
         kwargs_dct = {
             'parm_dirP_fileP_str': self.parm_dirP_str,
+            'py_caller_script_fileP_str': py_caller_script_fileP_str,
             'wrapper_bash_scrpt_fileP_str': self.wrapper_bash_scrpt_fileP_str,
             'cat_obj': self.cat_obj
         }
@@ -305,6 +354,6 @@ class MakeflowFromStages:
             makeflow_jx_creator_obj.add_rule(stage_obj.crt_makeflow_rule(**kwargs_dct))
 
         # Write out the makeflow file
-        os.makedirs(os.path.dirname(out_fileP_str), exist_ok=True)
-        with open(out_fileP_str, 'w') as file_obj:
+        os.makedirs(os.path.dirname(makeflow_out_fileP_str), exist_ok=True)
+        with open(makeflow_out_fileP_str, 'w') as file_obj:
             file_obj.write(makeflow_jx_creator_obj.get_str())
