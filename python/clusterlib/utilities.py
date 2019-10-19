a8687a96bceb358f524621cebac0d20af3b60db7
willemm <Willem.Marais@ssec.wisc.edu>
2019-10-19 19:40:15 +0000

    Added a function that flatten nested list and dictionaries.

--- python/clusterlib/utilities.py
+++ python/clusterlib/utilities.py
@@ -1,4 +1,22 @@
 import os
 
+
 def get_dirP_of__file__(__fileP_str__: str) -> str:
     return os.path.dirname(os.path.realpath(__fileP_str__))
+
+
+def flatten_list_dict(itr_obj):
+    """Flatten nested dictionaries and list."""
+
+    rtn_obj_lst = []
+    if isinstance(itr_obj, dict) is True:
+        rtn_obj_lst += flatten_list_dict(list(itr_obj.values()))
+
+    elif isinstance(itr_obj, list) is True:
+        for _iter_obj in itr_obj:
+            rtn_obj_lst += flatten_list_dict(_iter_obj)
+
+    else:
+        rtn_obj_lst = [itr_obj]
+
+    return rtn_obj_lst
