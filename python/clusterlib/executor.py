80ffcd01ca37b9bc5226445002998e32e3230a96
willemm <Willem.Marais@ssec.wisc.edu>
2019-10-21 14:11:41 +0000

    Fixed an issue where the input files listed in the executor parameter file where not properly identified.

--- python/clusterlib/executor.py
+++ python/clusterlib/executor.py
@@ -1,4 +1,5 @@
 import os
+import stat
 import copy
 import yaml
 import inspect
@@ -6,6 +7,7 @@ import secrets
 import importlib
 import clusterlib.file as Tfile
 import clusterlib.makeflow as makeflow
+from clusterlib.utilities import flatten_list_dict
 from typing import Callable, Dict, List, Tuple, Union
 
 
@@ -83,7 +85,8 @@ class Stage(StageAbstract):
                  name_str: str,
                  py_func: Callable,
                  input_parm_obj_dct: Dict[str, Union[int, float, str, StageFile, List[StageFile]]],
-                 output_file_dct: Dict[str, StageFile]):
+                 output_file_dct: Dict[str, StageFile],
+                 log_fileN_str: str):
         """
 
         Parameter
@@ -99,7 +102,9 @@ class Stage(StageAbstract):
         input_parm_obj_dct: dictionary of either int, float or str
             The named input parameters to the python function that will be called.
         output_fileP_str_dct: dict of str
-            List of named output files for the stage."""
+            List of named output files for the stage.
+        log_fileN_str: str
+            Name of the log file."""
 
         if inspect.isfunction(py_func) is False:
             err_str = 'The parameter "py_func" has to be a function.'
@@ -110,6 +115,7 @@ class Stage(StageAbstract):
         self.py_func = py_func
         self.input_parm_obj_dct = input_parm_obj_dct
         self.output_file_dct = output_file_dct
+        self.log_fileN_str = log_fileN_str
 
         # Make sure that the stage does not already exists in the graph
         if name_str in graph_stage_dct:
@@ -183,52 +189,78 @@ class Stage(StageAbstract):
 
         def transform_StageFile_objects(input_name_str, input_obj):
             _output_obj = None
-            _input_fileP_str_dct = dict()
-            _output_fileP_str_dct = dict()
+            _input_fileP_str_obj = None
+            _output_fileP_str_obj = None
 
             if isinstance(input_obj, StageInputFile) is True:
                 _output_obj = str(input_obj)
-                _input_fileP_str_dct[input_name_str] = str(input_obj)
+                _input_fileP_str_obj = str(input_obj)
 
             elif isinstance(input_obj, StageOutputFile) is True:
                 _output_obj = str(input_obj)
-                _output_fileP_str_dct[input_name_str] = str(input_obj)
+                _output_fileP_str_obj = str(input_obj)
 
             elif isinstance(input_obj, list) is True:
                 _output_obj = list()
+                _input_fileP_str_obj = list()
+                _output_fileP_str_obj = list()
                 for idx, _input_obj in enumerate(input_obj):
                     _input_name_str = f'{input_name_str}_{idx}'
-                    __output_obj, __input_fileP_str_dct, __output_fileP_str_dct = \
+                    __output_obj, __input_fileP_str_obj, __output_fileP_str_obj = \
                         transform_StageFile_objects(_input_name_str, _input_obj)
 
                     _output_obj.append(__output_obj)
-                    _input_fileP_str_dct.update(__input_fileP_str_dct)
-                    _output_fileP_str_dct.update(__output_fileP_str_dct)
+
+                    if __input_fileP_str_obj is not None:
+                        _input_fileP_str_obj.append(__input_fileP_str_obj)
+
+                    if __output_fileP_str_obj is not None:
+                        _output_fileP_str_obj.append(__output_fileP_str_obj)
+
+                if len(_input_fileP_str_obj) == 0:
+                    _input_fileP_str_obj = None
+
+                if len(_output_fileP_str_obj) == 0:
+                    _output_fileP_str_obj = None
 
             elif isinstance(input_obj, dict) is True:
                 _output_obj = dict()
+                _input_fileP_str_obj = dict()
+                _output_fileP_str_obj = dict()
                 for key_str, _input_obj in input_obj.items():
                     _input_name_str = f'{input_name_str}_{key_str}'
-                    __output_obj, __input_fileP_str_dct, __output_fileP_str_dct = \
+                    __output_obj, __input_fileP_str_obj, __output_fileP_str_obj = \
                         transform_StageFile_objects(_input_name_str, _input_obj)
 
                     _output_obj[key_str] = __output_obj
-                    _input_fileP_str_dct.update(__input_fileP_str_dct)
-                    _output_fileP_str_dct.update(__output_fileP_str_dct)
+
+                    if __input_fileP_str_obj is not None:
+                        _input_fileP_str_obj[key_str] = __input_fileP_str_obj
+
+                    if __output_fileP_str_obj is not None:
+                        _input_fileP_str_obj[key_str] = __output_fileP_str_obj
+
+                if len(_input_fileP_str_obj) == 0:
+                    _input_fileP_str_obj = None
+
+                if len(_output_fileP_str_obj) == 0:
+                    _output_fileP_str_obj = None
 
             else:
                 _output_obj = input_obj
 
-            return _output_obj, _input_fileP_str_dct, _output_fileP_str_dct
+            return _output_obj, _input_fileP_str_obj, _output_fileP_str_obj
 
         # Populate the keyword arguments of the function; also keep a list of input files
         for input_parm_name_str, input_parm_value_obj in self.input_parm_obj_dct.items():
-            output_obj, input_fileP_str_dct, output_fileP_str_dct = transform_StageFile_objects(input_parm_name_str,
+            output_obj, input_fileP_str_obj, output_fileP_str_obj = transform_StageFile_objects(input_parm_name_str,
                                                                                                 input_parm_value_obj)
 
             parm_dct['function_kwargs'][input_parm_name_str] = output_obj
-            parm_dct['input_fileP_str_dct'].update(input_fileP_str_dct)
-            parm_dct['output_fileP_str_dct'].update(output_fileP_str_dct)
+            if input_fileP_str_obj is not None:
+                parm_dct['input_fileP_str_dct'][input_parm_name_str] = input_fileP_str_obj
+            if output_fileP_str_obj is not None:
+                parm_dct['output_fileP_str_dct'][input_parm_name_str] = output_fileP_str_obj
 
         # Write out the parameter yaml file
         with Tfile.TFileTo(fileP_str=parm_fileP_str) as tfile_obj:
@@ -244,14 +276,27 @@ class Stage(StageAbstract):
         with open(py_caller_script_fileP_str, 'w') as file_obj:
             file_obj.write('import sys\n')
             file_obj.write('import clusterlib.executor as executor\n')
-            file_obj.write(f'executor.Stage.execute(sys.argv[1])\n')
+            file_obj.write('executor.Stage.execute(sys.argv[1])\n')
 
         # Create the command string
-        cmd_str = f'/bin/bash {wrapper_bash_scrpt_fileP_str} {py_caller_script_fileP_str} {parm_fileP_str}'
+        cmd_str = f'/bin/bash {wrapper_bash_scrpt_fileP_str} {py_caller_script_fileP_str}' \
+            + f' {parm_fileP_str} > {self.log_fileN_str} 2>&1'
 
         # Create the input and output file lists
-        input_fileP_str_lst = [parm_fileP_str] + list(parm_dct['input_fileP_str_dct'].values())
-        output_fileP_str_lst = list(parm_dct['output_fileP_str_dct'].values())
+        input_fileP_str_lst = [parm_fileP_str]
+        if parm_dct['input_fileP_str_dct'] is not None:
+            input_fileP_str_lst += flatten_list_dict(parm_dct['input_fileP_str_dct'])
+
+        output_fileP_str_lst = []
+        if parm_dct['output_fileP_str_dct'] is not None:
+            output_fileP_str_lst += flatten_list_dict(parm_dct['output_fileP_str_dct'])
+
+        # All of the items in `input_fileP_str_lst` should be strings
+        for input_fileP_str in input_fileP_str_lst:
+            if isinstance(input_fileP_str, str) is False:
+                err_str = 'Not all the items of `input_fileP_str` are strings:\nFrom {:s} to {:s}'
+                raise ValueError(err_str.format(str(parm_dct['input_fileP_str_dct']),
+                                                str(input_fileP_str_lst)))
 
         # Create the makeflow rule object
         mf_rule_obj = makeflow.Rule(category_obj=cat_obj)
@@ -278,14 +323,46 @@ class Stage(StageAbstract):
         # Create the yaml parameter config file that has the local file paths
         local_kwargs_param_dct = copy.deepcopy(param_dct['function_kwargs'])
 
-        # Create a list of transcended input files
-        in_tfile_obj_lst = []
-        for name_str, input_fileP_str in param_dct['input_fileP_str_dct'].items():
-            tfile_obj = Tfile.TFileFrom(fileP_str=input_fileP_str)
-            in_tfile_obj_lst.append(tfile_obj)
+        def tfile_from_flatten_list_dict(_itr_obj):
+            _tfile_obj_lst = []
 
-            if name_str in local_kwargs_param_dct:
-                local_kwargs_param_dct[name_str] = tfile_obj.local_fileP_str
+            if isinstance(_itr_obj, dict) is True:
+                _rtn_itr_obj = dict()
+                for _key_str, _value_obj in _itr_obj.items():
+                    _new_value_obj, __tfile_obj_lst = \
+                        tfile_from_flatten_list_dict(_value_obj)
+
+                    _rtn_itr_obj[_key_str] = _new_value_obj
+                    _tfile_obj_lst += __tfile_obj_lst
+
+                return _rtn_itr_obj, _tfile_obj_lst
+
+            elif isinstance(_itr_obj, list) is True:
+                _rtn_itr_obj = list()
+                for __itr_obj in _itr_obj:
+                    _new_itr_obj, __tfile_obj_lst = \
+                        tfile_from_flatten_list_dict(__itr_obj)
+
+                    _rtn_itr_obj.append(_new_itr_obj)
+                    _tfile_obj_lst += __tfile_obj_lst
+
+                return _rtn_itr_obj, _tfile_obj_lst
+
+            elif isinstance(_itr_obj, str) is True:
+                _tfile_obj = Tfile.TFileFrom(fileP_str=_itr_obj)
+
+                _tfile_obj_lst = [_tfile_obj]
+                _rtn_itr_obj = _tfile_obj.local_fileP_str
+
+                return _rtn_itr_obj, _tfile_obj_lst
+
+            else:
+                err_str = 'The given object has to be either a str, lst or dict.'
+                raise ValueError(err_str)
+
+        input_fileP_str_dct, in_tfile_obj_lst = tfile_from_flatten_list_dict(param_dct['input_fileP_str_dct'])
+        for key_str, value_obj in input_fileP_str_dct.items():
+            local_kwargs_param_dct[key_str] = value_obj
 
         # Create a list of transcended output files
         out_tfile_obj_lst = []
@@ -305,7 +382,11 @@ class Stage(StageAbstract):
 class MakeflowFromStages:
     """Create the makeflow file from the collection of stages."""
 
-    def __init__(self, parm_dirP_str: str, wrapper_bash_scrpt_fileP_str: str, cat_obj: makeflow.Category = None):
+    def __init__(self,
+                 parm_dirP_str: str,
+                 wrapper_bash_scrpt_fileP_str: str,
+                 cat_obj: makeflow.Category = None,
+                 yaml_cfg_dct: dict = None):
         """
 
         Parameters
@@ -313,13 +394,51 @@ class MakeflowFromStages:
         parm_dirP_str: str
             The parameter directory path of where to archive the parameter YAML files for the stage execution.
         wrapper_bash_scrpt_fileP_str: str
-            The iris bash wrapper script executable.
+            The iris bash wrapper script executable for the python script.
         cat_obj: makeflow.Category
-            A Makeflow category object; optional."""
+            A Makeflow category object; optional.
+        yaml_cfg_dct: dict
+            Eg. {
+                'command_options': {
+                    'shared_fs': {
+                        'option': '--shared-fs',
+                        'value': 'bla bla'
+                    }
+                }
+            }
+        """
 
         self.parm_dirP_str = parm_dirP_str
         self.wrapper_bash_scrpt_fileP_str = wrapper_bash_scrpt_fileP_str
         self.cat_obj = cat_obj
+
+        if yaml_cfg_dct is None:
+            self.yaml_cfg_dct = dict()
+        else:
+            self.yaml_cfg_dct = yaml_cfg_dct
+
+    def _get_command_options(self) -> dict:
+        if 'command_options' in self.yaml_cfg_dct:
+            return self.yaml_cfg_dct['command_options']
+        else:
+            return dict()
+
+    def _create_makeflow_bash_command(self, makeflow_fileP_str: str, out_fileP_str: str):
+        out_str = """#!/usr/bin/env bash
+
+makeflow"""
+
+        # Add makeflow options
+        command_options_dct = self._get_command_options()
+        for _, option_value_dct in command_options_dct.items():
+            option_str = option_value_dct['option']
+            value_str = option_value_dct['value']
+            out_str += f' {option_str} "{value_str}"'
+
+        out_str += f' --jx {makeflow_fileP_str}\n'
+
+        with open(out_fileP_str, 'w') as file_obj:
+            file_obj.write(out_str)
 
     def create(self,
                graph_stage_dct: Dict[str, Tuple[StageAbstract, StageAbstractCollection_type]],
@@ -357,3 +476,13 @@ class MakeflowFromStages:
         os.makedirs(os.path.dirname(makeflow_out_fileP_str), exist_ok=True)
         with open(makeflow_out_fileP_str, 'w') as file_obj:
             file_obj.write(makeflow_jx_creator_obj.get_str())
+
+        # Write the bash script thats execute the makeflow file
+        bash_makeflow_fileP_str = os.path.join(os.path.dirname(makeflow_out_fileP_str),
+                                               'run_' + os.path.basename(makeflow_out_fileP_str) + '.bash')
+        self._create_makeflow_bash_command(makeflow_out_fileP_str,
+                                           bash_makeflow_fileP_str)
+
+        # Make the makeflow bash script executable
+        st_obj = os.stat(bash_makeflow_fileP_str)
+        os.chmod(bash_makeflow_fileP_str, st_obj.st_mode | stat.S_IXUSR)
